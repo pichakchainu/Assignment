3d694064da64c5a67c4ecc33236a1cd1a6fffcfa
Ole <pchainu@manaosoftware.com>
2019-07-11 11:25:39 +0700

    - Add test validation

--- Assignment/Assignment.Test/IntegrationTests/Repositories/CustomerRepositoryTests.cs
+++ Assignment/Assignment.Test/IntegrationTests/Repositories/CustomerRepositoryTests.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using WebAPI.Infrastructure.Repositories;
 using WebAPI.Services;
 using Xunit;
@@ -19,15 +20,15 @@ namespace Assignment.Test.IntegrationTests.Repositories
             var customersAndTransactions = MockCustomers.CreateCustomersAndTransactions();
             Mock<ICustomerRepository> mockCustomerRepository = new Mock<ICustomerRepository>();
 
-            mockCustomerRepository.Setup(mr => mr.GetCustomerById(It.IsAny<int>())).Returns((int i) =>
+            mockCustomerRepository.Setup(mr => mr.GetCustomerByIdAsync(It.IsAny<int>())).ReturnsAsync((int i) =>
                 customers.Where(x => x.Id == i).Select(x => x.toCustomerDTO())
             );
 
-            mockCustomerRepository.Setup(mr => mr.GetCustomerDTOByEmail(It.IsAny<string>())).Returns((string email) =>
+            mockCustomerRepository.Setup(mr => mr.GetCustomerDTOByEmailAsync(It.IsAny<string>())).ReturnsAsync((string email) =>
                customersAndTransactions.Where(x => x.Email == email).Select(x => x.toCustomerAndOneTransactionDTO())
            );
 
-            mockCustomerRepository.Setup(mr => mr.GetCustomerDTOByIdAndEmail(It.IsAny<int>(), It.IsAny<string>())).Returns((int id, string email) =>
+            mockCustomerRepository.Setup(mr => mr.GetCustomerDTOByIdAndEmailAsync(It.IsAny<int>(), It.IsAny<string>())).ReturnsAsync((int id, string email) =>
                customersAndTransactions.Where(x => x.Id == id && x.Email == email).Select(x => x.toCustomerDTO())
            );
 
@@ -36,24 +37,24 @@ namespace Assignment.Test.IntegrationTests.Repositories
         }
 
         [Fact]
-        public void GetCustomerDTOById()
+        public async Task GetCustomerDTOById()
         {
-            var customers = this.MockCustomerRepository.GetCustomerById(0);
+            var customers = await MockCustomerRepository.GetCustomerByIdAsync(0);
             Assert.True(customers.Any());
         }
 
         [Fact]
-        public void GetCustomerDTOByEmail()
+        public async Task GetCustomerDTOByEmail()
         {
-            var customers = this.MockCustomerRepository.GetCustomerDTOByEmail("test1@gmail.com");
+            var customers = await MockCustomerRepository.GetCustomerDTOByEmailAsync("test1@gmail.com");
             Assert.True(customers.Any());
             Assert.True(customers.FirstOrDefault().Transactions.Count == 1);
         }
 
         [Fact]
-        public void GetCustomerDTOByIdAndEmail()
+        public async Task GetCustomerDTOByIdAndEmail()
         {
-            var customers = this.MockCustomerRepository.GetCustomerDTOByIdAndEmail(0, "test1@gmail.com");
+            var customers = await MockCustomerRepository.GetCustomerDTOByIdAndEmailAsync(0, "test1@gmail.com");
             Assert.True(customers.Any());
             Assert.True(customers.FirstOrDefault().Transactions.Count == 3);
 
---
+++ Assignment/Assignment.Test/IntegrationTests/Validations/ValidationCustomerTest.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebAPI.Services.Customers;
+using Xunit;
+
+namespace Assignment.Test.IntegrationTests.Validations
+{
+    public class ValidationCustomerTest
+    {
+        [Fact]
+        public void Validation_CustomerID_Should_MoreThanZero()
+        {
+            var service = new CustomerValidationService();
+            int value = 1;
+            var result = service.IsValidationId(value);
+            Assert.True(result.IsValid);
+        }
+
+        [Fact]
+        public void Validation_CustomerID_ShouldNotAllow_Zero()
+        {
+            var service = new CustomerValidationService();
+            int value = 0;
+            var result = service.IsValidationId(value);
+            Assert.False(result.IsValid);
+        }
+
+        [Fact]
+        public void Validation_CustomerID_ShouldNotAllow_Negative()
+        {
+            var service = new CustomerValidationService();
+            int value = -1;
+            var result = service.IsValidationId(value);
+            Assert.False(result.IsValid);
+        }
+
+        [Fact]
+        public void Validation_Email_NoInquiryCriteria()
+        {
+            var service = new CustomerValidationService();
+            string value = null;
+            string errorMessage = "";
+            var result = service.IsValidationEmail(value);
+            foreach (var error in result.Errors)
+            {
+                errorMessage = error.ErrorMessage;
+            }
+            Assert.False(result.IsValid);
+            Assert.Equal("No inquiry criteria", errorMessage);
+        }
+
+        [Fact]
+        public void Validation_Email_FormatNoTCorrect()
+        {
+            var service = new CustomerValidationService();
+            string value = "test";
+            string errorMessage = "";
+            var result = service.IsValidationEmail(value);
+            foreach (var error in result.Errors)
+            {
+                errorMessage = error.ErrorMessage;
+            }
+            Assert.False(result.IsValid);
+            Assert.Equal("Invalid Email", errorMessage);
+        }
+
+        [Fact]
+        public void Validation_Email_FormatIsCorrect()
+        {
+            var service = new CustomerValidationService();
+            string value = "test@gmail.com";
+            var result = service.IsValidationEmail(value);
+
+            Assert.True(result.IsValid);
+        }
+
+        [Fact]
+        public void Validation_IDandEmail_Data_IsCorrect()
+        {
+            var service = new CustomerValidationService();
+            int valueID = 1;
+            string valueEmail = "test@gmail.com";
+            var result = service.IsValidationIdAndEmail(valueID, valueEmail);
+
+            Assert.True(result.IsValid);
+        }
+
+        [Fact]
+        public void Validation_IDandEmail_DataBoth_IsNotCorrect()
+        {
+            var service = new CustomerValidationService();
+            int valueID = 0;
+            string valueEmail = "test", messageErrorId = "", messageErrorEmail = "";
+            var result = service.IsValidationIdAndEmail(valueID, valueEmail);
+            foreach (var error in result.Errors)
+            {
+                if (error.PropertyName == "id")
+                    messageErrorId = error.ErrorMessage;
+
+                if (error.PropertyName == "email")
+                    messageErrorEmail = error.ErrorMessage;
+            }
+            Assert.False(result.IsValid);
+            Assert.Equal("Invalid Customer ID", messageErrorId);
+            Assert.Equal("Invalid Email", messageErrorEmail);
+        }
+
+        [Fact]
+        public void Validation_IDandEmail_Email_NotFillIn()
+        {
+            var service = new CustomerValidationService();
+            int valueID = 1;
+            string valueEmail =null, messageErrorId = "", messageErrorEmail = "";
+            var result = service.IsValidationIdAndEmail(valueID, valueEmail);
+            foreach (var error in result.Errors)
+            {
+                if (error.PropertyName == "id")
+                    messageErrorId = error.ErrorMessage;
+
+                if (error.PropertyName == "email")
+                    messageErrorEmail = error.ErrorMessage;
+            }
+            Assert.False(result.IsValid);
+            Assert.NotEqual("Invalid Customer ID", messageErrorId);
+            Assert.Equal("No inquiry criteria", messageErrorEmail);
+        }
+    }
+}
